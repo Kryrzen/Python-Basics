699a94fda9ffc8970b22d33e2705ff6a6ce6fe96
Kryrzen <vino.kugen@gmail.com>
2026-01-20 09:04:13 -0500

    Added passhasher

---
+++ DayTwo/passhasher.py
@@ -0,0 +1,14 @@
+import hashlib
+
+# Get User Input
+text = input("Enter the word you want to hash: ")
+
+# Generate MD5 Hash
+md5_hash = hashlib.md5(text.encode()).hexdigest()
+print(f"MD5H Hash:{md5_hash}")
+
+# Append to passwords.txt
+with open("password.txt", "a") as file:
+    file.write(text + "\n")
+
+print(f"'{text}' has been added to password.txt")
